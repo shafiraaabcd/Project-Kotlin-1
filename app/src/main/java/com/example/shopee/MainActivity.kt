8c06ea9969361495eacd0a417d0975025552cf19
shafiraaaabcd <fitrianinda_shafira_30rpl@student.smktelkom-mlg.sch.id>
2023-02-02 17:10:58 +0700

    berhasil

---
+++ app/src/main/java/com/example/shopee/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.shopee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (supportActionBar != null) {
+            (supportActionBar as ActionBar).title = "PINKFLASH OFFICIAL STORE"
+        }
+    }
+}
